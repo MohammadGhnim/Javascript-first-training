40f5ac7ac8b28f2b48e4db50f00411b82732777c
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-01-19 17:16:31 +0000

    add-new

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ function listArrayItems(arr) {
         }
     }
 }
+
+/* Exercise */
+
+function letterFinder (word, match) {
+    for (var i = 0; i < word.length; i++) {
+        if (word[i] == match) {
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
+    }
+}
+letterFinder("test", "t")

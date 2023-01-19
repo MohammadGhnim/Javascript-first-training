41150d1b44faf33829eba73cacc1aa8a06c19beb
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-01-19 14:31:39 +0000

    edit1

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function listArrayItems(arr){
 }
 var colors = ['red', 'orange', 'yello', 'green', 'blue', 'purple', 'pink'];
 listArrayItems(colors);
+
+/* to start the count from one instead of zero */
+
+function listArrayItems(arr){
+    for (var i = 0; i < arr.length; i++){
+        console.log(i + 1, arr[i])
+    }
+}

5629f1caadb8cffd793b290365350a71b215963d
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-01-19 17:31:50 +0000

    add-comment

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function listArrayItems(arr){
 var colors = ['red', 'orange', 'yello', 'green', 'blue', 'purple', 'pink'];
 listArrayItems(colors);
 
-/* to start the count from one instead of zero */
+// to start the count from one instead of zero //
 
 function listArrayItems(arr){
     for (var i = 0; i < arr.length; i++){
@@ -14,7 +14,7 @@ function listArrayItems(arr){
     }
 }
 
-/* add another condition */
+// add another condition //
 
 function listArrayItems(arr) {
     for (var i = 0; i < arr.length; i++){
@@ -26,7 +26,28 @@ function listArrayItems(arr) {
     }
 }
 
-/* Exercise */
+/* Exercise: 
+Task 1:
+Write a function named letterFinder that accepts two parameters: word and match.
+
+Task 2:
+Code a 'for' loop inside the function's body. The for loop's counter should start at zero, increment by 1 on each iteration and exit when the counter variable's value is equal to the length of the word parameter.
+
+Task 3:
+Add an if statement inside the for loop whose condition works as follows:
+
+Access each of the letters inside the passed in word using the counter variable, with word[i]. 
+
+Check if the current word[i] is equal to the value of match.
+
+Task 4:
+console.log the following inside the body of the if statement: console.log('Found the', match, 'at', i).
+
+Task 5:
+Write the else condition. Here you'll just console log the following: console.log('---No match found at', i).
+
+Task 6:
+Call the letterFinder and pass it as its first argument as the string "test" and as its second argument, the string "t". */
 
 function letterFinder (word, match) {
     for (var i = 0; i < word.length; i++) {

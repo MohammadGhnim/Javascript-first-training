d1c69645d6f4cfaa8306168b72977116765e5480
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-01-19 14:24:49 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function listArrayItems(arr){
+    for (var i = 0; i < arr.length; i++){
+        console.log(i, arr[i])
+    }
+}
+var colors = ['red', 'orange', 'yello', 'green', 'blue', 'purple', 'pink'];
+listArrayItems(colors);

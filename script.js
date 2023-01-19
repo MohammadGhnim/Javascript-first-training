a9169593a9694b41a1e16d16b3c06a771bb4338f
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-01-19 14:40:07 +0000

    edit2

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function listArrayItems(arr){
         console.log(i + 1, arr[i])
     }
 }
+
+/* add another condition */
+
+function listArrayItems(arr) {
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] == 'red') {
+            console.log(i*100, "tomato!")
+        } else {
+            console.log(i*100, arr[i])
+        }
+    }
+}

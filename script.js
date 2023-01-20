2132c4e723ad04e04379995f3147857847132751
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-01-20 12:43:28 +0100

    Math-ceil

--- script.js
+++ script.js
@@ -188,3 +188,26 @@ console.log(decimal);
 // log the value of decimal MUTIPLIED BY 10
 console.log(decimal * 10);
 
+//Math.ceil()
+
+// Rounded up any decimal number to the nearest integer
+// var rounded = Math.ceil(0.0001);
+// var rounded = Math.ceil(0.5);
+// var rounded = Math.ceil(0.99);
+// var rounded = Math.ceil(1.01);
+// var rounded = Math.ceil(1.5);
+// var rounded = Math.ceil(2.99);
+
+console.log(rounded);
+
+// Random integer
+
+/* Get a random decimal number between 0 and 0.99 ...
+and save it to a variable */
+var decimal = Math.random() * 10;
+
+// Rounding up the value of decimal 
+var rounded = Math.ceil(decimal);
+
+// log the value of rounded to the console
+console.log(rounded);

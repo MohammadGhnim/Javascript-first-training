1559d33a885bebf9e1d318e67905321abce34491
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-01-20 14:50:00 +0100

    more

--- script.js
+++ script.js
@@ -211,3 +211,47 @@ var rounded = Math.ceil(decimal);
 
 // log the value of rounded to the console
 console.log(rounded);
+
+
+// For loop over arrays 
+var letters = ['a', 'b', 'c'];
+for (var i = 0; i< letters.length; i++ ){
+    console.log(letters[i]);
+}
+
+// For loop over string 
+var letters = "abc";
+for (var i=0; i < letters.length; i++){
+    console.log(letters[i]);
+}
+
+// example:
+var veggies = ['onion', 'parsley', 'carrot'];
+console.log(veggies.length);
+
+console.log(veggies[0]);
+console.log(veggies[1]);
+
+for (var i = 0; i < veggies.length; i++) {
+    console.log(veggies[i]);
+}
+
+// example:
+var greeting = 'Howdy';
+console.log(greeting.length);
+
+console.log(greeting[0]);
+console.log(greeting[1]);
+
+for (var i = 0; i < greeting.length; i++) {
+    console.log(greeting[i]);
+}
+
+// example:
+var greet = 'Hello';
+var user = 'Lisa';
+
+// console.log(greet.pop())            // get error
+
+console.log(greet + user);            //Hello Lisa
+console.log(greet.concat(user));     //Hello Lisa

934a086882c9af33e22637c535b7b9c1f48f811e
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-01-20 16:40:28 +0100

    more2

--- script.js
+++ script.js
@@ -280,3 +280,36 @@ greet.charAt(0);  // 'H'
 // to change the casing of strings:
 greet.toUpperCase();       // "HELLO, "
 greet.toLowerCase();       // "hello, "
+
+/* Task:
+Create a new empty array literal and assign it to the variable clothes.
+
+Add 5 of your favorite items of clothing as strings using the push() method.
+
+Remove the fifth piece of clothing from the array using the pop() method.
+
+Add a new piece of clothing using the push() method.
+
+Use console.log to show the third item from the clothes array in the console.
+
+Create a new empty object literal and assign it to the variable favCar.
+
+Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+
+Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+
+Use the console to log the entire favCar object. */
+
+var clothes = [];
+clothes.push("T-shirt");
+clothes.push("Sweater");
+clothes.push("Jacket");
+clothes.push("Coat");
+clothes.push("Jeans");
+clothes.pop();
+clothes.push("Socks");
+console.log(clothes[2]);
+var favCar = {};
+favCar.color = "black";
+favCar.covertible = 4;
+console.log(favCar);

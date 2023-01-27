cba9df6102e1bf3e79cfce0508eb2443ffb029ac
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-01-27 12:33:46 +0100

    inheritance

--- script.js
+++ script.js
@@ -1358,3 +1358,67 @@ baldEagle.useWings(); // "Flying! Barely flapping!"
 kingPenguin.useWings(); // "Diving!"
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------                           Constructors                                         --------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------                           Inheritance                                           -------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// prototype:
+var bird = {
+    hasWings: true,
+    canFly: true,
+    hasFeathers: true
+}
+
+var eagle1 = Object.create(bird);
+console.log("eagle1: ", eagle1);
+//-----> eagle1: {}
+
+console.log("eagle1 has wings:", eagle1.hasWings);
+console.log("eagle1 can fly:", eagle1.canFly);
+console.log("eagle1 has feathers:", eagle1.hasFeathers);
+/*
+----> eagle1:  {}
+----> eagle1 has wings: true
+---->eagle1 can fly: true
+----> eagle1 has feathers: true
+ */
+
+var eagle2 = Object.create(bird);
+console.log("eagle2 has wings:", eagle2.hasWings);
+/* 
+----> eagle1:  {}
+----> eagle1 has wings: true
+---->eagle1 can fly: true
+----> eagle1 has feathers: true
+----> eagle2 has wings: true 
+*/
+
+var penguin1 = Object.create(bird);
+penguin1.canFly = false;
+console.log("penguin1: ", penguin1);
+/*
+----> eagle1:  {}
+----> eagle1 has wings: true
+---->eagle1 can fly: true
+----> eagle1 has feathers: true
+----> eagle2 has wings: true 
+----> penguin1:  { canFly: false }
+*/
+console.log("penguin1 has feathers:", penguin1.hasFeathers);
+console.log("penguin1 can fly: ", penguin1.canFly);
+/*
+----> eagle1:  {}
+----> eagle1 has wings: true
+----> eagle1 can fly: true
+----> eagle1 has feathers: true
+----> eagle2 has wings: true 
+----> penguin1:  { canFly: false }
+----> penguin1 has feathers: true
+----> penguin1 can fly:  false
+*/
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
